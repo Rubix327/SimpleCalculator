8047cea79edb8b9b16e3ed65d58600591dd6bd8f
unknown <rubix3327@gmail.com>
2019-11-24 19:48:49 +0300

    Wrote a very simple calculator GUI

--- main.py
+++ main.py
@@ -1,4 +1,41 @@
-x = int(input())
-y = int(input())
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
 
-operation_division = x/y
+
+root = tk.Tk()
+root.title("Калькулятор")
+
+memory = ''
+
+def calc(key):
+    global memory
+    if key == '=':
+        eval(memory)
+    else:
+        memory += key
+
+
+buttons = [
+    'CE' , 'C' , '<<', '/',
+    '7' , '8' , '9', '*',
+    '4' , '5' , '6', '-',
+    '1' , '2' , '3', '+',
+    '+/-', '0' , ',', '='
+]
+
+r = 1
+c = 0
+
+for btn in buttons:
+    tk.rel = ""
+    tk.cmd = lambda x=btn: calc(x)
+    ttk.Button(root, text=btn).grid(row=r,column=c)
+    c += 1
+    if c>3:
+        r += 1
+        c = 0
+
+calcEntry = tk.Entry(root,width=33)
+calcEntry.grid(row=0,column=0,columnspan=5)
+root.mainloop()
